df4cafb9dcd1103c3ee5f902e66e59815285e07e
Ryan Castner <castner.rr@gmail.com>
2019-01-01 23:28:12 -0500

    feat(blogpost): Styling tweaks

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
+import { media } from '../utils/media';
+import { Layout } from '../components/Layout';
 
 type Props = {
   data: {
@@ -8,14 +11,31 @@ type Props = {
   };
 };
 
+const StyledArticle = styled.article`
+  ${media.sm`max-width: 90%;`}
+  ${media.md`max-width: 80%;`}
+  ${media.lg`max-width: 70%;`}
+  ${media.xl`max-width: 60%;`}
+`;
+
+const H1 = styled.h1`
+  color: ${props => props.theme.colors.cyan};
+`;
+
+const Meta = styled.p`
+  color: ${props => props.theme.colors.grey};
+  font-size: 12px;
+`;
+
 const BlogPost: React.SFC<Props> = ({ data: { markdownRemark: post } }) => (
-  <div>
-    <Helmet title={`${post.frontmatter.title} | Castner.Tech`} />
-    <div>
-      <h1>{post.frontmatter.title}</h1>
+  <Layout>
+    <Helmet title={`${post.frontmatter.title} | castner.tech`} />
+    <StyledArticle>
+      <H1>{post.frontmatter.title}</H1>
+      <Meta>{post.frontmatter.date}</Meta>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </div>
-  </div>
+    </StyledArticle>
+  </Layout>
 );
 
 export const query = graphql`
