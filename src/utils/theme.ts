b13bf34d13198e3b5ac27316073d34a5d3a9a4b9
Ryan Castner <castner.rr@gmail.com>
2019-01-01 23:26:35 -0500

    refactor(theme): Remove unused theme props

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -14,8 +14,4 @@ export const theme = {
     background: '#16242c',
     link: '#005bbb',
   },
-  space: [0, 4, 8, 16, 32, 64, 128, 256],
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
-  breakpoints: Object.keys(sizes).map(key => sizes[key]),
-  fontWeights: [400, 600, 800],
 };
